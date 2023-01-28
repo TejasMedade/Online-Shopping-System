f48d2b9882709f8da65694c8337ad54bb51fac44
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-01-28 14:40:09 +0530

    Update User.java

--- Online Shopping Application/src/main/java/com/masai/model/User.java
+++ Online Shopping Application/src/main/java/com/masai/model/User.java
@@ -5,12 +5,11 @@ package com.masai.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -27,23 +26,21 @@ public class User {
 
 	// Id should be Contact Number
 	@Id
-	@NotBlank(message="User.contact.null")
-	@NotEmpty(message="User.contact.null")
-	@NotNull(message="User.contact.null")
-	@Min(value = 10 , message="User.contact.invalid")
+	@NotNull(message = "{User.id.invalid}")
+	@NotBlank(message = "{User.id.invalid}")
+	@NotEmpty(message = "{User.id.invalid}")
+	@Size(min = 10, max = 12, message = "{User.id.invalid}")
 	private String id;
-	
-	@NotBlank(message="User.password.invalid")
-	@NotEmpty(message="User.password.invalid")
-	@NotNull(message="User.password.invalid")
-	@Min(value = 6, message = "{User.password.invalid}")
-	@Max(value = 8, message = "{User.password.invalid}")
-	@Pattern(regexp = "^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\\w\\d\\s:])([^\\s]){6,8}$", message = "{Admin.password.invalid}")
+
+	@NotNull(message = "{User.password.invalid}")
+	@NotBlank(message = "{User.password.invalid}")
+	@NotEmpty(message = "{User.password.invalid}")
+	@Pattern(regexp = "^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\\w\\d\\s:])([^\\s]){6,12}$", message = "{Customer.password.invalid}")
 	private String password;
 
 	// Choose From Admin or Customer
-	@NotBlank(message="User.role.invalid")
-	@NotEmpty(message="User.role.invalid")
-	@NotNull(message="User.role.invalid")
+	@NotNull(message = "{User.role.invalid}")
+	@NotBlank(message = "{User.role.invalid}")
+	@NotEmpty(message = "{User.role.invalid}")
 	private String role;
 }
