aabd7c214d4715c8a892d10eb0f7f760c59b031c
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-01-28 14:39:59 +0530

    Update ProductService.java

--- Online Shopping Application/src/main/java/com/masai/services/ProductService.java
+++ Online Shopping Application/src/main/java/com/masai/services/ProductService.java
@@ -6,9 +6,9 @@ package com.masai.services;
 import java.util.List;
 
 import com.masai.exceptions.AdminException;
-import com.masai.exceptions.CustomerException;
 import com.masai.exceptions.LoginException;
 import com.masai.exceptions.ProductException;
+import com.masai.exceptions.UserException;
 import com.masai.model.Product;
 
 /**
@@ -21,17 +21,19 @@ public interface ProductService {
 	public List<Product> viewallProducts() throws ProductException;
 
 	// Admin Role
-	public Product insertProduct(String key, Product product) throws LoginException, AdminException;
+	public Product insertProduct(String userid,String userpassword , String key, Product product)
+			throws LoginException, AdminException, UserException;
 
 	// Admin Role
-	public Product updateProduct(String key, Product product)
-			throws LoginException, ProductException, AdminException;
+	public Product updateProduct(String userId,String userPassword, String key, Product product)
+			throws AdminException, LoginException, ProductException, UserException;
 
 	public Product viewProduct(Integer productId) throws ProductException;
 
 	public List<Product> viewProductByCategory(String category_Name) throws ProductException;
 
 	// Admin Role
-	public String removeProduct(String key, Integer product_Id) throws LoginException, ProductException, AdminException;
+	public String removeProduct(String userId,String userPassword, String key, Integer product_Id)
+			throws AdminException, LoginException, ProductException, UserException;
 
 }
