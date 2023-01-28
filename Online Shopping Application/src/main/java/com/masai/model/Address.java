37fd430cf84982fab98a58a8427c23550e4be848
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-01-28 14:40:12 +0530

    Update Address.java

--- Online Shopping Application/src/main/java/com/masai/model/Address.java
+++ Online Shopping Application/src/main/java/com/masai/model/Address.java
@@ -7,10 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,9 +25,6 @@ import lombok.NoArgsConstructor;
 public class Address {
 
 	@Id
-	@NotNull
-	@NotBlank
-	@NotEmpty
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer addressId;
 
@@ -49,25 +46,25 @@ public class Address {
 	@NotNull(message = "{Address.city.invalid}")
 	@NotBlank(message = "{Address.city.invalid}")
 	@NotEmpty(message = "{Address.city.invalid}")
-	@Min(value = 3, message = "{Address.city.invalid}")
+	@Size(min = 3, message = "{Address.city.invalid}")
 	private String city;
 
 	@NotNull(message = "{Address.state.invalid}")
 	@NotBlank(message = "{Address.state.invalid}")
 	@NotEmpty(message = "{Address.state.invalid}")
-	@Min(value = 3, message = "{Address.state.invalid}")
+	@Size(min = 3, message = "{Address.city.invalid}")
 	private String state;
 
 	@NotNull(message = "{Address.country.invalid}")
 	@NotBlank(message = "{Address.country.invalid}")
 	@NotEmpty(message = "{Address.country.invalid}")
-	@Min(value = 3, message = "{Address.country.invalid}")
+	@Size(min = 3, message = "{Address.city.invalid}")
 	private String country;
 
 	@NotNull(message = "{Address.pincode.invalid}")
 	@NotBlank(message = "{Address.pincode.invalid}")
 	@NotEmpty(message = "{Address.pincode.invalid}")
-	@Min(value = 6, message = "{Address.pincode.invalid}")
+	@Size(min = 3, message = "{Address.city.invalid}")
 	private String pincode;
 
 }
