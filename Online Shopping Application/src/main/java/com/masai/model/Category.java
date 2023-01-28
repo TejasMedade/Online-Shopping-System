320d86ba00d1040c10ee3c6bd8bd30f15964c9e0
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-01-28 14:40:31 +0530

    Update Category.java

--- Online Shopping Application/src/main/java/com/masai/model/Category.java
+++ Online Shopping Application/src/main/java/com/masai/model/Category.java
@@ -4,11 +4,10 @@
 package com.masai.model;
 
 import javax.persistence.Embeddable;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,13 +25,12 @@ public class Category {
 	@NotNull(message="{Category.id.invalid}")
 	@NotBlank(message="{Category.id.invalid}")
 	@NotEmpty(message="{Category.id.invalid}")
-	@Min(value = 4, message = "{Category.id.invalid}")
-	@Max(value = 4, message = "{Category.id.invalid}")
+	@Size(min = 4, max = 5, message = "{Category.id.invalid}")
 	private Integer catId;
 	
-	@NotNull(message="{Category.name.invalid}")
-	@NotBlank(message="{Category.name.invalid}")
-	@NotEmpty(message="{Category.name.invalid}")
+	@NotNull(message = "{Category.name.invalid}")
+	@NotBlank(message = "{Category.name.invalid}")
+	@NotEmpty(message = "{Category.name.invalid}")
 	private String categoryName;
 
 }
