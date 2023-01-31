667d94506b364b0b64cecdd9acfcd7571deeacaa
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-02-01 01:52:15 +0530

    Update ProductController.java

--- Online Shopping Application/src/main/java/com/masai/controllers/ProductController.java
+++ Online Shopping Application/src/main/java/com/masai/controllers/ProductController.java
@@ -5,12 +5,15 @@ package com.masai.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +22,9 @@ import com.masai.exceptions.AdminException;
 import com.masai.exceptions.CustomerException;
 import com.masai.exceptions.LoginException;
 import com.masai.exceptions.ProductException;
+import com.masai.exceptions.UserException;
 import com.masai.model.Product;
 import com.masai.services.ProductService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 /**
  * @author tejas
@@ -45,26 +47,28 @@ public class ProductController {
 
 	// Admin Role
 	@PostMapping("/addProduct")
-	public ResponseEntity<Product> addProductHandler(@RequestParam String key, @RequestBody Product product)
-			throws CustomerException, LoginException, AdminException {
+	public ResponseEntity<Product> addProductHandler(@RequestParam String key, @Valid @RequestBody Product product,
+			@Valid @RequestParam String userid, @Valid @RequestParam String userpassword)
+			throws CustomerException, LoginException, AdminException, UserException {
 
-		Product added_product = productService.insertProduct(key, product);
+		Product added_product = productService.insertProduct(userid, userpassword, key, product);
 
 		return new ResponseEntity<Product>(added_product, HttpStatus.ACCEPTED);
 	}
 
 	// Admin Role
 	@PutMapping("/updateProduct")
-	public ResponseEntity<Product> updateProductHandler(@RequestParam String key, @RequestBody Product product)
-			throws LoginException, ProductException, AdminException {
+	public ResponseEntity<Product> updateProductHandler(@Valid @RequestParam String userId,
+			@Valid @RequestParam String userPassword, @RequestParam String key, @Valid @RequestBody Product product)
+			throws LoginException, ProductException, AdminException, UserException {
 
-		Product updated_product = productService.updateProduct(key, product);
+		Product updated_product = productService.updateProduct(userId, userPassword, key, product);
 
 		return new ResponseEntity<Product>(updated_product, HttpStatus.ACCEPTED);
 	}
 
 	@GetMapping("/viewProduct")
-	public ResponseEntity<Product> viewProductHandler(@RequestParam Integer productId) throws ProductException {
+	public ResponseEntity<Product> viewProductHandler(@Valid @RequestParam Integer productId) throws ProductException {
 
 		Product product = productService.viewProduct(productId);
 
@@ -72,7 +76,7 @@ public class ProductController {
 	}
 
 	@GetMapping("/viewProductByCategory")
-	public ResponseEntity<List<Product>> viewProductByCategoryHandler(@RequestParam String category_Name)
+	public ResponseEntity<List<Product>> viewProductByCategoryHandler(@Valid @RequestParam String category_Name)
 			throws ProductException {
 
 		List<Product> listofproducts = productService.viewProductByCategory(category_Name);
@@ -82,11 +86,12 @@ public class ProductController {
 	}
 
 	// Admin Role
-	@GetMapping("/deleteProduct")
-	public ResponseEntity<String> removeProductHandler(@RequestParam String key, @RequestParam Integer product_Id)
-			throws CustomerException, LoginException, ProductException, AdminException {
+	@PostMapping("/deleteProduct")
+	public ResponseEntity<String> removeProductHandler(@Valid @RequestParam String userId,
+			@Valid @RequestParam String userPassword, @RequestParam String key, @Valid @RequestParam Integer product_Id)
+			throws CustomerException, LoginException, ProductException, AdminException, UserException {
 
-		String result = productService.removeProduct(key, product_Id);
+		String result = productService.removeProduct(userId, userPassword, key, product_Id);
 
 		return new ResponseEntity<String>(result, HttpStatus.OK);
 
